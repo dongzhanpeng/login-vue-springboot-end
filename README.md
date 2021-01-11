490a898a2e605ad99ab2eeff7c292ec5bc9330a2
zhanpeng.dong <zhanpeng.dong@southgis.com>
2021-01-11 15:01:28 +0800

    【*】添加工程说明及SQL

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # login-vue-springboot-end
-This is a back-end login system 
+环境搭建：
+
+----后端：
+
+--------JDK8
+
+--------Mysql
+
+--------Maven
+
+--------IDEA
+
+--------Sqlyog
+
+--------postman
+
+项目git之后，打开IDEA，maven自动导入jar包之后，即可运行，若想debug自我调试，安装Sqlyog，导入login.sql文件，即可使用postman进行调试
---
+++ login.sql
@@ -0,0 +1,36 @@
+/*
+SQLyog Ultimate v11.24 (32 bit)
+MySQL - 5.7.27 : Database - login_test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`login_test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `login_test`;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键标识',
+  `username` varchar(100) NOT NULL COMMENT '用户名',
+  `password` varchar(100) NOT NULL COMMENT '密码',
+  `email` varchar(100) DEFAULT NULL COMMENT '邮箱',
+  `captcha` varchar(100) DEFAULT NULL COMMENT '验证码',
+  `phonenumber` varchar(100) DEFAULT NULL COMMENT '手机号',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
